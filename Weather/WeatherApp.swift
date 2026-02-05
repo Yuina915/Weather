81217477a0688618170fa5790ab13dea2a3b2f67
Yuina Okajima <yuina2006@icloud.com>
2026-02-06 04:51:33 +0900

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by 岡島結南 on 2026/02/06.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
